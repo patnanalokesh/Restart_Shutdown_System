f8817491a39782a67efbe9d007c8c9324d130282
patnanalokesh <109522655+patnanalokesh@users.noreply.github.com>
2022-09-01 10:39:24 +0530

    Add files via upload

---
+++ Restarting_System.py
@@ -0,0 +1,5 @@
+# For Restarting System
+# import os
+# os.system('shutdown /r /t 0')
+
+# it's for system restarting code using "Python"
---
+++ Shutdown_System.py
@@ -0,0 +1,5 @@
+# For Shutdown the System
+# import os
+# os.system("shutdown /s /t 1")
+
+# it's for system shutdown code using "Python"
